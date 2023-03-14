7da0b8c04412a74459976f8c35802513080fcdb5
AliSoliman203 <ali.soool1997@gmail.com>
2023-03-14 16:47:26 +0200

    update

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import styled from "styled-components";
 import {NavbarSection, Logo, LogoText, Ulist, ListItem, Anchor, Input, Label} from './style'
 
 
 const Navbar = () => {
+
+    const StyledLink = styled(Link)`
+        display: block;
+        color: #222;
+        text-decoration: none;
+        padding: 10px 15px;
+        font-weight: bold;
+        &:hover {
+            color: #eb5424;
+        }
+    `
 
     return (
         <NavbarSection>
@@ -18,12 +30,12 @@ const Navbar = () => {
                 <Label htmlFor="nav"></Label>
                 
                 <Ulist>
-                    <ListItem><Link className="iXYjvU" to="/react-app1/">Home</Link></ListItem>
+                    <ListItem><StyledLink to="/react-app1/">Home</StyledLink></ListItem>
                     <ListItem><Anchor>Work</Anchor></ListItem>
                     <ListItem><Anchor>Portfolio</Anchor></ListItem>
                     <ListItem><Anchor>Resume</Anchor></ListItem>
                     <ListItem><Anchor>About</Anchor></ListItem>
-                    <ListItem><Link className="iXYjvU" to="/contact">Contact</Link></ListItem>
+                    <ListItem><StyledLink to="/contact">Contact</StyledLink></ListItem>
                 </Ulist>
                 
             </div>
